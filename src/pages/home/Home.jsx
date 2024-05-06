bd833233ed4044fafd8480386cb737978af769ef
MarwenCode <marwen_jdidi@yahoo.com>
2024-05-06 23:25:28 +0200

    add part style for home mobile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,6 @@ import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import data from "../../data/data.json";
-
-
 import './home.scss';
 import { Card } from '../../components/card/Card';
 
